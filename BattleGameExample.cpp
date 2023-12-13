09d8667635c04b468783cf89bbb0d5d0040871ea
TamerAAli <tali@zewailcity.edu.eg>
2023-12-13 07:02:32 +0200

    Updated code of Lec 2

--- BattleGameExample.cpp
+++ BattleGameExample.cpp
@@ -1,20 +1,43 @@
-// BattleGameExample.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+enum ArmyUnitType { Soldier, AirCraft, Tank, Undefined };
+
+class ArmyUnit
+{
+protected:
+	int strength = 0;
+	int health = 0;
+	int ammu = 0;
+	ArmyUnitType unitType = Undefined;
+
+public:
+	virtual bool fireAt(ArmyUnit* target)
+	{
+		if (
+			target != nullptr &&
+			this->isAlive() &&
+			this->ammu > 0 &&
+			target->isAlive() &&
+			this->canFireAt(target)
+			)
+		{
+			this->ammu--;
+			target->takeDamageFrom(this);
+		}
+	}
+	bool isAlive() const { return this->health > 0; }
+	virtual bool canFireAt(ArmyUnit* target) = 0;
+	bool takeDamageFrom(ArmyUnit* enemy)
+	{
+		if (this->canTakeDamageFrom(enemy))
+		{
+			this->health -= min(enemy->strength, this->health);
+		}
+	}
+	virtual bool canTakeDamageFrom(ArmyUnit* enemy) = 0;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
